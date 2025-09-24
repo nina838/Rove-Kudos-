c1a599dde6278c61aa660c031b335c34d1532f66
nina838 <nadaames57@gmail.com>
2025-09-24 23:04:58 +0400

    Update Wall.jsx
    delete duplicate wall.jsx

--- src/src/pages/src/pages/Wall.jsx
+++ src/src/pages/src/pages/Wall.jsx
@@ -1,11 +1,1 @@
-// src/pages/Wall.jsx
-import React from "react";
 
-export default function Wall() {
-  return (
-    <div style={{ padding: 24 }}>
-      <h2>Wall page placeholder</h2>
-      <p>If you still see this, App.jsx is importing ./pages/Wall.jsx. We’ll remove that next.</p>
-    </div>
-  );
-}
